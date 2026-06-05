458b4161ce31548b97fc6ae4d66bee48a3e5814e
Ivan Belkov <ivan-belkov2@yandex.ru>
2026-06-05 17:21:14 +0300

    fix: web hash history

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import WeekPage from '../pages/WeekPage.vue';
 import YearPage from '../pages/YearPage.vue';
 
@@ -8,7 +8,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/todo/'), // соответствует base в vite.config.js
+  history: createWebHashHistory('/todo/'), // ← хеш-режим
   routes,
 });
 
